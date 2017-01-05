d69bcb1af713bf46d89f9a8d7df6dffa3b9bb7f2
josephcode773 <josephcode773@gmail.com>
2017-01-05 13:24:22 -0600

    Final, Submitted Version

--- 04_whereforeArtThou.js
+++ 04_whereforeArtThou.js
@@ -3,8 +3,8 @@ function whatIsInAName(collection, source) {
     // What's in a name?
 	//var arr = [];
     // Only change code below this line
-	var arr = collection.filter(function (item) {
-		for(var i in source) {
+	const arr = collection.filter(function (item) {
+		for(let i in source) {
 			if(source[i] != item[i]) {
 				return false;
 			}
@@ -12,7 +12,8 @@ function whatIsInAName(collection, source) {
 		return true;
 	});
     // Only change code above this line
-	return arr;
+	//return arr;
+	console.log(arr);
 }
 
 whatIsInAName([{ 'a': 1, 'b': 2 }, { 'a': 1 }, { 'a': 1, 'b': 2, 'c': 2 }], { 'a': 1, 'b': 2 });
