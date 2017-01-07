ac264d97400394e8a0d4735ed66a027a0553389c
Joe Henry <Joe Henry>
2017-01-07 17:33:39 -0600

    from one workstation to another

--- 07_dnaPairing.js
+++ 07_dnaPairing.js
@@ -4,18 +4,31 @@ Task 1: Split 'str', create DNA pair array, create 2d array structure.
 
 function pairElement(str) {
 	let arr = str.split('');
-	let basedPaired = {
-		A: 'T',
-		T: 'A',
-		C: 'G',
-		G: 'C'
+	let dnaPairs = {
+		A: ['A', 'T'],
+		T: ['T', 'A'],
+		C: ['C', 'G'],
+		G: ['G', 'C']
 	};
-	let myarray=new Array(5);
-	for (let i=0; i <5; i++)
-		myarray[i]=new Array(2);
+	let arr2 = []
+	// dnaPairs.hasOwnProperty(arr[i])
+	// dnaPairs.hasOwnProperty(A)
+	//arr[0] == dnaPairs[]
+	debugger;
+	for (var i = 0; i < arr.length; i++) {
+		//let i = value that 0 index represents
+		j = arr[i];
+		if (dnaPairs.hasOwnProperty(j)) {
+			arr2.push(dnaPairs.j)
+		}
+	}
+
+	// let myarray=new Array(5);
+	// for (let i=0; i <5; i++)
+	// 	myarray[i]=new Array(2);
 
 	//return str;
-	console.log(arr);
+	console.log(arr2);
 }
 
 pairElement('GCG');
