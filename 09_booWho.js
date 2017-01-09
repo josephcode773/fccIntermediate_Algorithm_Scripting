2a3be9f68d3e6fa64c47a0a9a7b02f49dfad524b
josephcode773 <josephcode773@gmail.com>
2017-01-09 12:29:33 -0600

    Check if a value is classified as a boolean primitive

--- 09_booWho.js
+++ 09_booWho.js
@@ -1,10 +1,18 @@
 /*
-* Initial Commit.
+* Task 1: Check if a value is classified as a boolean primitive.
 * */
 
 function booWho(bool) {
     // What is the new fad diet for ghost developers? The Boolean.
-	return bool;
+	if (bool === true || bool === false) {
+	    bool = true;
+	} else {
+	    bool = false;
+	}
+
+	//bool = bool <= 0;
+	//return bool;
+	console.log(bool);
 }
 
-booWho(null);
+booWho(true);
