372d0895f204627f86ffdcc2bec76a4f17c69e14
josephcode773 <josephcode773@gmail.com>
2017-01-10 00:17:20 -0600

    Use .replace on str

--- 11_convertHTML.js
+++ 11_convertHTML.js
@@ -1,11 +1,26 @@
 /*
-* Initial Commit
+* Task 1: Use .replace on str
 * */
 
 
 function convertHTML(str) {
-  // &colon;&rpar;
-  return str;
+	//return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
+	str = str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
+	console.log(str);
+	//return str;
 }
 
-convertHTML("Dolce & Gabbana");
+convertHTML("abc");
+
+//return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
+
+/*
+convertHTML("Dolce & Gabbana") should return Dolce &​amp; Gabbana.
+convertHTML("Hamburgers < Pizza < Tacos") should return Hamburgers &​lt; Pizza &​lt; Tacos.
+convertHTML("Sixty > twelve") should return Sixty &​gt; twelve.
+convertHTML('Stuff in "quotation marks"') should return Stuff in &​quot;quotation marks&​quot;.
+convertHTML("Shindler's List") should return Shindler&​apos;s List.
+convertHTML("<>") should return &​lt;&​gt;.
+convertHTML("abc") should return abc.
+
+*/
