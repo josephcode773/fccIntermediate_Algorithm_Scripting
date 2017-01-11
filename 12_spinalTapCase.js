44e8646a52c1a1b62b9eb2489ce8845b9c593aa8
josephcode773 <josephcode773@gmail.com>
2017-01-10 21:12:39 -0600

    Remove Spaces

--- 12_spinalTapCase.js
+++ 12_spinalTapCase.js
@@ -1,11 +1,18 @@
 /*
-* Initial Commit
+* Task 1: Revmoe Spaces
 * */
 
 function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins
-  return str;
+
+  //return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
+
+	let tapTap = str.replace(/ /g, '-').replace(/_/g, '-');
+	console.log(tapTap);
+	
+  //.replace(/ /g,"_")
+  //return str;
 }
 
-spinalCase('This Is Spinal Tap');
+spinalCase('thisIsSpinalTap');
