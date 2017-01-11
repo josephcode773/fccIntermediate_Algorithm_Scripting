274a5b2c52b9093282ce59f5f65c3b582e06b8ae
josephcode773 <josephcode773@gmail.com>
2017-01-11 09:52:51 -0600

    Add toLowerCase to end of 'str'

--- 12_spinalTapCase.js
+++ 12_spinalTapCase.js
@@ -1,15 +1,12 @@
 /*
-* Task 2: Add toLowerCase to 'str'
+* Task 3: Add toLowerCase to end of 'str'
 * */
 
 function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins
 
-  str =  str.toLowerCase().replace(/ /g, '-').replace(/_/g, '-').replace(/([a-z])([A-Z])/g,'$1' + '-' + '$2');
-	//console.log(holder.toLowerCase());
-  console.log(str);
-  //return str;
+	return str =  str.replace(/ /g, '-').replace(/_/g, '-').replace(/([a-z])([A-Z])/g,'$1' + '-' + '$2').toLowerCase();
 }
 
-spinalCase('This Is Spinal Tap');
+spinalCase('AllThe-small Things');
