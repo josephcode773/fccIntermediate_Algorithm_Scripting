b37f3abeebc45b6b91ac4fbe8e8aac3ade4025ee
josephcode773 <josephcode773@gmail.com>
2017-01-11 09:32:31 -0600

    Remove Spaces and add hyphen

--- 12_spinalTapCase.js
+++ 12_spinalTapCase.js
@@ -1,18 +1,28 @@
 /*
-* Task 1: Revmoe Spaces
+* Task 1: Remove Spaces and add hyphen
 * */
 
 function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins
 
-  //return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
-
-	let tapTap = str.replace(/ /g, '-').replace(/_/g, '-');
-	console.log(tapTap);
+	// let tapTap = str.replace(/ /g, '-').replace(/_/g, '-');
+	// console.log(tapTap);
 	
+	// function upperToHyphenLower(match) {
+	//   return '-' + match.toLowerCase();
+	// }
+
+	// function lowerHyphenUpper(match) {
+	// 	return '-' + match.toLowerCase();
+	// }
+  // var holder =  str.replace(/[A-Z]/g, upperToHyphenLower);
+  //var holder =  str.replace(/([a-z])([A-Z])/g,'$1' + '-' + '$2');
+	var holder =  str.replace(/ /g, '-').replace(/_/g, '-').replace(/([a-z])([A-Z])/g,'$1' + '-' + '$2');
+  
+	console.log(holder);
   //.replace(/ /g,"_")
   //return str;
 }
 
-spinalCase('thisIsSpinalTap');
+spinalCase('This Is Spinal Tap');
