707f1f3f462beddaa4aad742c595ba45a5d3b33c
josephcode773 <josephcode773@gmail.com>
2017-01-11 22:15:47 -0600

    Made arr of fibonacci

--- 13_sumAllOdd.js
+++ 13_sumAllOdd.js
@@ -1,9 +1,21 @@
 /*
-* Initial Commit
+* Task 1: Made arr of fibonacci
 * */
 
 function sumFibs(num) {
-  return num;
+	let nummy = '';
+	let arr = [1, 1, 3, 5];
+	for (var i = 0; i < num; i++) {
+		arr[i + 2] = arr[i + 1] + arr[i];
+	}
+	function lessThanNum(value) {
+		return value <= num;
+	}
+	
+	var filtered = arr.filter(lessThanNum);
+	//return num;
+	console.log(arr);
+
 }
 
-sumFibs(4);
+sumFibs(10);
