00f7224f3a97af66c623c7d088aa06f6a66ec1fc
josephcode773 <josephcode773@gmail.com>
2017-01-15 15:12:08 -0600

    Add all Primes...

--- 14_sumAllPrimes.js
+++ 14_sumAllPrimes.js
@@ -1,5 +1,5 @@
 /*
-* Task 2: Test each number for primeness
+* Task 3: Add all Primes...
 * */
 
 function sumPrimes(num) {
@@ -8,81 +8,32 @@ function sumPrimes(num) {
 		numArray.push(j);
 	}
 	numArray.forEach(function(element) {
-		if (element > 2 && element % 2 === 0) {
+		var squElement = Math.round(Math.sqrt(element));
+		if (element > 2 && element % squElement === 0) {
 			numArray.splice(numArray.indexOf(element), 1);
 		}
 	});
-	numArray.forEach(function(element) {
-		if (element > 3 && element % 3 === 0) {
-			numArray.splice(numArray.indexOf(element), 1);
-		}
-	});
-	numArray.forEach(function(element) {
-		if (element > 5 && element % 5 === 0) {
-			numArray.splice(numArray.indexOf(element), 1);
-		}
-	});
-	numArray.forEach(function(element) {
-		if (element > 7 && element % 7 === 0) {
-			numArray.splice(numArray.indexOf(element), 1);
-		}
-	});	
-		// if (element % 3 === 0) {
-		// 	numArray.splice(numArray.indexOf(element), 1);
-		// }
-		// if (element % 5 === 0) {
-		// 	numArray.splice(numArray.indexOf(element), 1);
-		// }
-		// if (element % 7 === 0) {
-		// 	numArray.splice(numArray.indexOf(element), 1);
-		// }
-	
-
-	// function oddNumbers(params) {
-	// 	return params % 2 == 1;
-	// }
-	// let numArray2 = numArray.filter(oddNumbers);
-	// numArray2.unshift(2);
-
-	// for (var k = 0; k < numArray2.length; k++) {
-	// 	var holder = numArray2[k];
-	// 	var yaya = Math.sqrt(holder);
-	// 	for (var i = 2; i <= yaya; i++) {
-	// 		var element = holder % i;
-	// 		//console.log(element);
-	// 		if (element === 0) {
-	// 			//console.log("This is NOT prime");
-	// 			numArray2.splice(numArray2.indexOf(holder), 1);
-	// 			//break;
-	// 		}
-	// 	}
-	// }
-
-	// numArray2.forEach(function (params) {
-	// 	let yaya = Math.sqrt(params);
-	// 	for (var i = 2; i <= yaya; i++) {
-	// 		var element = params % i;
-	// 		//console.log(element);
-	// 		if (element === 0) {
-	// 			//console.log("This is NOT prime");
-	// 			numArray2.splice(numArray.indexOf(params), 1);
-	// 			break;
-	// 		}
+	// numArray.forEach(function(element) {
+	// 	if (element > 3 && element % 3 === 0) {
+	// 		numArray.splice(numArray.indexOf(element), 1);
 	// 	}
 	// });
-
-	// let yaya = Math.sqrt(num);
-	// for (var i = 2; i <= yaya; i++) {
-	// 	var element = num % i;
-	// 	console.log(element);
-	// 	if (element === 0) {
-	// 		console.log("This is NOT prime");
-	// 		break;
+	// numArray.forEach(function(element) {
+	// 	if (element > 5 && element % 5 === 0) {
+	// 		numArray.splice(numArray.indexOf(element), 1);
 	// 	}
-	// }
-
+	// });
+	// numArray.forEach(function(element) {
+	// 	if (element > 7 && element % 7 === 0) {
+	// 		numArray.splice(numArray.indexOf(element), 1);
+	// 	}
+	// });	
+	function addEmUp(a, b) {
+		return a + b;
+	}
+	let numArray2 = numArray.reduce(addEmUp,0);
 	console.log(numArray);
 	//return num;
 }
 
-sumPrimes(89);
+sumPrimes(127);
