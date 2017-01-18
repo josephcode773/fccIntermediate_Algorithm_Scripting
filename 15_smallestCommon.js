1a5b350ffc8e1883666b66c28274f09da0ee3f34
josephcode773 <josephcode773@gmail.com>
2017-01-18 08:08:35 -0600

    new formula for lcm

--- 15_smallestCommon.js
+++ 15_smallestCommon.js
@@ -6,60 +6,76 @@
  * - test if locate arr2 values are in arr3...
  */
 
-function smallestCommons(arr) {
-	var arr = [5,4,3,2,1]
-	var holder = [];
+// function smallestCommons(arr) {
+// 	// var arr = [5,4,3,2,1];
+// 	// var holder = [];
+//     //
+// 	// let puff0 = arr[0];
+// 	// let puff0Array = [];
+// 	// for (let i = puff0; i < 100000; i+=puff0) {
+// 	// 	puff0Array.push(i);
+// 	// }
+//     //
+// 	// let puff1 = arr[1];
+// 	// let puff1Array = [];
+// 	// for (let i = puff1; i < 100000; i+=puff1) {
+// 	// 	puff1Array.push(i);
+// 	// }
+//     //
+// 	// let puff2 = arr[2];
+// 	// let puff2Array = [];
+// 	// for (let i = puff2; i < 100000; i+=puff2) {
+// 	// 	puff2Array.push(i);
+// 	// }
+//     //
+// 	// let puff3 = arr[3];
+// 	// let puff3Array = [];
+// 	// for (let i = puff3; i < 100000; i+=puff3) {
+// 	// 	puff3Array.push(i);
+// 	// }
+//     //
+// 	// let puff4 = arr[4];
+// 	// let puff4Array = [];
+// 	// for (let i = puff4; i < 100000; i+=puff4) {
+// 	// 	puff4Array.push(i);
+// 	// }
+//
+//     // holder = puff1Array.filter(puff0Array);
+//
+// 	function LCM(A)
+//     {
+// 		var n = A.length, a = Math.abs(A[0]);
+// 		for (var i = 1; i < n; i++)
+//         { var b = Math.abs(A[i]), c = a;
+// 			while (a && b){ a > b ? a %= b : b %= a; }
+// 			a = Math.abs(c*A[i])/(a+b);
+// 		}
+// 		return a;
+// 	}
+//
+//     /* For example:
+//      LCM([-50,25,-45,-18,90,447]) -> 67050
+//      */
+//
+// 	//return arr;
+// 	console.log(a);
+// }
+//
+// LCM([1,2,3,4,5]);
+// //smallestCommons([1,5]);
 
-	let puff0 = arr[0];
-	let puff0Array = [];
-	for (let i = puff0; i < 100000; i+=puff0) {
-		puff0Array.push(i);
+
+function LCM(A) {
+	var n = A.length, a = Math.abs(A[0]);
+	for (var i = 1; i < n; i++)
+    { var b = Math.abs(A[i]), c = a;
+		while (a && b){ a > b ? a %= b : b %= a; }
+		a = Math.abs(c*A[i])/(a+b);
 	}
-
-	let puff1 = arr[1];
-	let puff1Array = [];
-	for (let i = puff1; i < 100000; i+=puff1) {
-		puff1Array.push(i);
-	}
-
-	let puff2 = arr[2];
-	let puff2Array = [];
-	for (let i = puff2; i < 100000; i+=puff2) {
-		puff2Array.push(i);
-	}
-
-	let puff3 = arr[3];
-	let puff3Array = [];
-	for (let i = puff3; i < 100000; i+=puff3) {
-		puff3Array.push(i);
-	}
-
-	let puff4 = arr[4];
-	let puff4Array = [];
-	for (let i = puff4; i < 100000; i+=puff4) {
-		puff4Array.push(i);
-	}
-
-	holder = puff1Array.filter(function lookItUp(element) {
-		for (var i = 0; i < puff0Array.length; i++) {
-			return puff0Array[i] === element
-			
-		}
-		
-	});
-
-	// let holder = [];
-	// arr.find(callback[, thisArg])
-	//var new_array = arr.filter(callback[, thisArg])
-	// function multiplesOf(params) {
-	// 	for (var i = 0; i < 100; i = i + params) {
-	// 		var element = array[i];
-			
-	// 	}
-	// }
-	//return arr;
-	console.log(holder);
+	return a;
 }
 
 
-smallestCommons([1,5]);
+//return arr;
+
+LCM([1,2,3,4,5]);
