34bb002f507fca51e2deeb5409200603e7e4f847
josephcode773 <josephcode773@gmail.com>
2017-01-18 12:34:28 -0600

    More clean

--- 15_smallestCommon.js
+++ 15_smallestCommon.js
@@ -1,95 +1,27 @@
 /**
  * Plan of Action:
- * - convert arr to 2 seperate arrays of multiples of value
- * - test if locate arr1[0](then arr1[2], etc.) in arr2 exist
- * - if true, then place in new array 
- * - test if locate arr2 values are in arr3...
  */
 
-// function smallestCommons(arr) {
-// 	// var arr = [5,4,3,2,1];
-// 	// var holder = [];
-//     //
-// 	// let puff0 = arr[0];
-// 	// let puff0Array = [];
-// 	// for (let i = puff0; i < 100000; i+=puff0) {
-// 	// 	puff0Array.push(i);
-// 	// }
-//     //
-// 	// let puff1 = arr[1];
-// 	// let puff1Array = [];
-// 	// for (let i = puff1; i < 100000; i+=puff1) {
-// 	// 	puff1Array.push(i);
-// 	// }
-//     //
-// 	// let puff2 = arr[2];
-// 	// let puff2Array = [];
-// 	// for (let i = puff2; i < 100000; i+=puff2) {
-// 	// 	puff2Array.push(i);
-// 	// }
-//     //
-// 	// let puff3 = arr[3];
-// 	// let puff3Array = [];
-// 	// for (let i = puff3; i < 100000; i+=puff3) {
-// 	// 	puff3Array.push(i);
-// 	// }
-//     //
-// 	// let puff4 = arr[4];
-// 	// let puff4Array = [];
-// 	// for (let i = puff4; i < 100000; i+=puff4) {
-// 	// 	puff4Array.push(i);
-// 	// }
-//
-//     // holder = puff1Array.filter(puff0Array);
-//
-// 	function LCM(A)
-//     {
-// 		var n = A.length, a = Math.abs(A[0]);
-// 		for (var i = 1; i < n; i++)
-//         { var b = Math.abs(A[i]), c = a;
-// 			while (a && b){ a > b ? a %= b : b %= a; }
-// 			a = Math.abs(c*A[i])/(a+b);
-// 		}
-// 		return a;
-// 	}
-//
-//     /* For example:
-//      LCM([-50,25,-45,-18,90,447]) -> 67050
-//      */
-//
-// 	//return arr;
-// 	console.log(a);
-// }
-//
-// LCM([1,2,3,4,5]);
-// //smallestCommons([1,5]);
 
-
-function LCM(A) {
+function smallestCommons(arr) {
 	// Formula to sort from Smallest to Largest Number
-	A.sort(function (a,b) {
+	arr.sort(function (a,b) {
 		return a - b;
 	});
 	// Formula to get all values between Smallest and Largest Numbers
 	let fullArr = [];
-	for (let i = A[0]; i <= A[1]; i++) {
+	for (let i = arr[0]; i <= arr[1]; i++) {
 		fullArr.push(i);	
 	}
-	A = fullArr;
+	arr = fullArr;
 	// Formula to get LCD
-	var n = A.length, a = Math.abs(A[0]);
-	for (var i = 1; i < n; i++)
-    { var b = Math.abs(A[i]), c = a;
-		while (a && b){ a > b ? a %= b : b %= a; }
-		a = Math.abs(c*A[i])/(a+b);
+	var n = arr.length, numOne = Math.abs(arr[0]);
+	for (var i = 1; i < arr.length; i++) { 
+		var numTwo = Math.abs(arr[i]), origNumOne = numOne;
+		while (numOne && numTwo){ numOne > numTwo ? numOne %= numTwo : numTwo %= numOne; }
+		numOne = Math.abs(origNumOne*arr[i])/(numOne+numTwo);
 	}
-	
-
-	console.log(a);
-//	return a;
+	return numOne;
 }
 
-
-//return arr;
-
-LCM([23,18]);
+smallestCommons([23,18]);
