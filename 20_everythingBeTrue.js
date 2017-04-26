35435983dac91198277a41cd6b0e73138e9cfa66
josephcode773 <josephcode773@gmail.com>
2017-04-26 09:34:49 -0500

    20 looking for way to use 2nd arg in map

--- 20_everythingBeTrue.js
+++ 20_everythingBeTrue.js
@@ -1,28 +1,13 @@
 
 function truthCheck(collection, pre) {
-	//console.log( collection);
-	// let arry = collection;
-	// console.log(arry);
-	// let arry2 = collection[0];
-	// Object.keys(arry2).forEach(function (key) {
-	// 	let obj = arry2[key];
-	// 	// do something with obj
-	// });
-
-	// for (oblong of collection) {
-	// 	console.log(Boolean(oblong.sex));
-	// }
+	var holder = pre;
 	var timbo = collection.map(function (winky) {
-		if (Boolean(winky.sex)) {
-			return true
-		} else {
-			return false;
-			//break;
-		}
+		return Boolean(winky.sex);
 	});
-	console.log(timbo)
+	return (timbo.includes(false)) ? false : true;
+	//console.log((timbo.includes(false)) ? false : true);
 }
 
-truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+truthCheck([{'user': 'Tinky-Winky', 'sex': 'male'}, {'user': 'Dipsy'}, {'user': 'Laa-Laa', 'sex': 'female'}, {'user': 'Po', 'sex': 'female'}], 'sex');
 
 
